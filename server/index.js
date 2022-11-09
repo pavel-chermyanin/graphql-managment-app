48f9a9152a781887b8fe3cf8a83ea1c4bd9b4625
Pavel <pavel_chermyanin@mail.ru>
2022-11-09 20:21:12 +0300

    configured the base sctructure of the app and connection to Mongo database, described ClientType and ProjectType into RootQuery, also Client and Project models

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const colors = require("colors");
+const { graphqlHTTP } = require("express-graphql");
+require("dotenv").config();
+const schema = require("./schema/schema.js");
+const connectDB = require('./config/db.js')
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Connect to Database
+connectDB()
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
+
+app.listen(port, console.log("Server running on port " + port));
